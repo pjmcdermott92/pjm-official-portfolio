ab9fd6de53cd526d428f797cebdcc838a04ea483
McDermott <patrick@pjmcdermott.com>
2023-04-25 12:20:21 -0700

    add hero and featured work sections

---
+++ src/components/Homepage/FeaturedWorkSection/FeaturedWorkSection.jsx
@@ -0,0 +1,93 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaGithub, FaGlobe } from 'react-icons/fa';
+import s from './FeaturedWorkSection.module.scss';
+
+// SAMPLE PORTFOLIO DATA - WILL BE REPLACED WITH AN API CALL FUNCTION IN PROD
+const featuredProjects = [
+    {
+        title: 'Raven Heating & Cooling, LLC',
+        short_description:
+            '<p>Raven Heating & Cooling, LLC is a family-owned HVAC business located in Tucson, AZ. Services provided include custom website design and development using NextJS.</p>',
+        github_link: 'https://github.com/pjmcdermott92',
+        website_url: 'https://ravenac.com',
+        project_image: '/ravenac-screenshot.png'
+    },
+    {
+        title: 'Devslopes Code Challenge',
+        short_description:
+            "<p>Sales funnel page for Devslopes Academy's newest 30-day Code Challenge using NextJS.</p>",
+        github_link: 'https://github.com/pjmcdermott92',
+        website_url: 'https://30daycodechallenge.com',
+        project_image: '/devslopes-screenshot.png'
+    },
+    {
+        title: "Amy's Grooming Salon",
+        short_description:
+            "<p>Amy's Grooming Salon is located in Atlanta, GA</p>",
+        github_link: 'https://github.com/pjmcdermott92',
+        website_url: 'https://pjmcdermott.com',
+        project_image: '/amys-grooming-screenshot.png'
+    }
+];
+
+export default function FeaturedWorkSection() {
+    return (
+        <section className={s.featuredWorkSection}>
+            <div className='container'>
+                <h2 className='section-title'>Featured Work</h2>
+
+                {featuredProjects?.length ? (
+                    <div className='card-row'>
+                        {featuredProjects.map((project) => (
+                            <div
+                                key={project.title}
+                                className={`card ${s.projectCard}`}
+                            >
+                                <div className='card__img'>
+                                    <Image
+                                        src={project.project_image}
+                                        alt={project.title}
+                                        fill
+                                        sizes='(max-width: 768px) 100vw,
+                                            (max-width: 1200px) 50vw,
+                                            33vw'
+                                        priority
+                                    />
+                                </div>
+                                <div className={`card__body ${s.card__body}`}>
+                                    <h4>
+                                        <Link href='/'>{project.title}</Link>
+                                    </h4>
+                                    <div
+                                        dangerouslySetInnerHTML={{
+                                            __html: project.short_description
+                                        }}
+                                    />
+                                    <div className={s.actionBtns}>
+                                        <Link
+                                            href={project.github_link}
+                                            className='btn btn-sm btn-outline-light'
+                                        >
+                                            <FaGithub />
+                                            View on Githb
+                                        </Link>
+                                        <Link
+                                            href={project.website_url}
+                                            className='btn btn-sm btn-outline-light'
+                                        >
+                                            <FaGlobe />
+                                            View Live Site
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <p>No projects to show.</p>
+                )}
+            </div>
+        </section>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,26 @@
 import Head from 'next/head';
-import { Inter } from '@next/font/google';
 import HeroSection from '@/components/Homepage/HeroSection/HeroSection';
-
-const inter = Inter({ subsets: ['latin'] });
+import FeaturedWorkSection from '@/components/Homepage/FeaturedWorkSection/FeaturedWorkSection';
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>Patrick McDermott - Full-Stack Developer</title>
-				<meta
-					name='description'
-					content='Full-stack web developer, building websites and web app that deliver an exceptional user experience.'
-				/>
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<main id='content'>
-        <HeroSection />
-      </main>
-		</>
-	);
+    return (
+        <>
+            <Head>
+                <title>Patrick McDermott - Full-Stack Developer</title>
+                <meta
+                    name='description'
+                    content='Full-stack web developer, building websites and web app that deliver an exceptional user experience.'
+                />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <main id='content'>
+                <HeroSection />
+				<FeaturedWorkSection />
+            </main>
+        </>
+    );
 }
